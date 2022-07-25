bb5640fb7ebce37c54eae6a578e111c9b3f92f37
thais <thaisviana081@outlook.com>
2022-07-25 09:31:48 -0300

    2. Teste o componente <About.js />. 🤗 ✔

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../pages/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <About.js />.', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const text = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(text).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const paragrafo = screen.getAllByText(/pokémons/i);
+    expect(paragrafo).toHaveLength(2);
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const img = screen.getByAltText(/pokédex/i);
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(img.src).toBe(url);
+  });
+});
